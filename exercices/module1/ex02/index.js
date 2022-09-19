84db36b7126047b2ffa6cf99994dd33439ed0754
Sikawie <mariam.miclauri@student.vinci.be>
2022-09-19 12:48:54 +0200

    maj

--- exercices/module1/ex02/index.js
+++ exercices/module1/ex02/index.js
@@ -1,5 +1,4 @@
 const dateTimeNow = new Date();
 console.log(dateTimeNow.toLocaleDateString()); // 17/08/2020
 console.log(dateTimeNow.toLocaleTimeString()); // 13:26:15
-alert(dateTimeNow.toLocaleDateString());
-alert(dateTimeNow.toLocaleTimeString());
+alert(dateTimeNow.toLocaleDateString() + " " + dateTimeNow.toLocaleTimeString());
