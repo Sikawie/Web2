0e6d338c2ec2efea85906a313491dca45520b844
Sikawie <mariam.miclauri@student.vinci.be>
2022-09-22 12:21:57 +0200

    ytresyuiolkjhgfdx

--- exercices/module1/ex03/index.js
+++ exercices/module1/ex03/index.js
@@ -1,18 +1,20 @@
 const clicks = document.querySelector('#CLICK');
-const compteurClicks = document.getElementById('#cpt');
+const compteurClicks = document.getElementById('cpt');
 
 let clicksNumber = 0;
 
 clicks.addEventListener('click', () => {
-    clicks++;
+    clicksNumber++;
 
-    if(clicks >=5 && clicks <=9){
-        clicks.innerHTML= "Bravo, bel échauffement !";
+    if (clicksNumber >= 5 && clicksNumber <= 9) {
+        clicks.innerHTML = "Bravo, bel échauffement !";
     }
-    
-    if(clicks >=10){
-        clicks.innerHTML= "Vous êtes passé maître en l'art du clic !";
+
+    if (clicksNumber >= 10) {
+        clicks.innerHTML = "Vous êtes passé maître en l'art du clic !";
     }
-    
-  console.log('onClickHandlerForBtn1::click');
+
+    compteurClicks.innerHTML = clicksNumber;
+
+    console.log('onClickHandlerForBtn1::click');
 });
