e76aeeebdfe4021d7f03255e901c1aeaa31a4079
Sikawie <mariam.miclauri@student.vinci.be>
2022-09-23 11:13:39 +0200

    UWU

--- exercices/module1/ex04/index.js
+++ exercices/module1/ex04/index.js
@@ -1,15 +1,16 @@
 const lightSequence = ['red', 'orange', 'green', 'orange'];
-const delayBerweenChanges = 1000;
+const delaysBetweenLightChanges = 1000;
 let currentLightIndex = 0;
 
 cycleThroughLights();
 
-function cycleThroughLights (){
-    const currentLightColor = lightSequence[currentLightIndex];
-    const currentLight = document.querySelector(`.${currentLightColor}`);
-    currentLight.style.backgroundColor = currentLightColor;
-    setTimeout(() => {
-        currentLight.style.backgroundColor = '';
-        currentLightIndex = (currentLightIndex + 1) 
-    })
+function cycleThroughLights() {
+  const currentLightColor = lightSequence[currentLightIndex];
+  const currentLight = document.querySelector(`.${currentLightColor}`);
+  currentLight.style.backgroundColor = currentLightColor;
+  setTimeout(() => {
+    currentLight.style.backgroundColor = '';
+    currentLightIndex = (currentLightIndex + 1) % lightSequence.length;
+    cycleThroughLights();
+  }, delaysBetweenLightChanges);
 }
